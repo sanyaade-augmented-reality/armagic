b69ed5dc5a7a07a41e06d844f4e4ba6c8ea904ff
leochatain <leochatain@22892e45-cd4f-0d29-0166-6a0decb81ae3>
2010-05-27 04:29:25 +0000

    Really basic classes, needs several revisions. Classes added: Card, CreatureCard, LandCard, Tracker and Camera.

---
+++ trunk/armagic/CreatureCard.h
@@ -0,0 +1,13 @@
+#ifndef ARMAGIC_CREATURE_CARD_H_
+#define ARMAGIC_CREATURE_CARD_H_
+
+class CreatureCard : public Card {
+public:
+	CreatureCard();
+	virtual ~CreatureCard();
+
+protected:
+private:
+};
+
+#endif
