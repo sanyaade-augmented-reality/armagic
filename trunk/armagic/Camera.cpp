93e806b8aab751f180b5835483cf71103c837086
leochatain <leochatain@22892e45-cd4f-0d29-0166-6a0decb81ae3>
2010-05-28 03:24:43 +0000

    Working on Camera class

---
+++ trunk/armagic/Camera.cpp
@@ -0,0 +1,31 @@
+#include "Camera.h"
+
+using namespace track;
+
+Camera::Camera(const std::string& videoConfig, const std::string& cparamPath) {
+	ARParam wparam;
+
+	videoConfig_ = videoConfig;
+	cparamPath_ = cparamPath;
+	const int c = 8;
+	if (arVideoOpen(const_cast<char*>(videoConfig_.c_str())) < 0)
+		throw Exception("arVideoOpen exception");
+	if (arVideoInqSize(&sizex_, &sizey_) < 0)
+		throw Exception("arVideoInqSize exception");
+	if (arParamLoad(cparamPath_.c_str(), 1, &wparam) < 0)
+		throw Exception("arParamLoad exception");
+	arParamChangeSize(&wparam, sizex_, sizey_, &cparam_);
+	arInitCparam(&cparam_);
+}
+
+Camera::~Camera() {
+	arVideoCapStop();
+	arVideoClose();
+	argCleanup();
+}
+
+ARUint8* Camera::getFrame() {
+	while ( (frame_ = (ARUint8*)arVideoGetImage()) == NULL )
+		arUtilSleep(2);
+	return frame_;
+}
