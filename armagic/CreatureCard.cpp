0fd4cb44b2182ab1447c1c7572b3aa748d6666ea
pmdusso <pmdusso@22892e45-cd4f-0d29-0166-6a0decb81ae3>
2010-06-07 02:04:00 +0000

    Moved to the master class Card the variables of scale and position. Implemented in the CreateCard and LandCard constructor the complete creation of a new card. Removed the unnecessary "setXXX" methods.

--- armagic/CreatureCard.cpp
+++ armagic/CreatureCard.cpp
@@ -2,15 +2,22 @@
 
 using std::string;
 
-CreatureCard::CreatureCard(const std::string& name, const int attack, const int defense, const int colorlessCost, const int colorCost,
-						   const int scaleX, const int scaleY, const int scaleZ, const int positionX, const int positionY, const int positionZ)
+CreatureCard::CreatureCard(Color color, const	std::string marker, const std::string model, const std::string texture, const bool isCreature,
+						   const std::string& name, const int attack, const int defense, const int colorlessCost, const int colorCost,
+						   const double scaleX, const double scaleY, const double scaleZ, const double positionX, const double positionY, const double positionZ)
 {
+	color_ = color;
+	marker_ = marker;
+	model_ = model;
+	texture_ = texture;
+	isCreature_ = isCreature;
+
 	name_ = name;
 	attack_ = attack;
 	defense_ = defense;
 	colorlessCost_ = colorlessCost;
 	colorCost_ = colorCost;
-
+	
 	scaleX_ = scaleX;
 	scaleY_ = scaleY;
 	scaleZ_ = scaleZ;
--- armagic/LandCard.cpp
+++ armagic/LandCard.cpp
@@ -2,9 +2,21 @@
 
 using std::string;
 
-LandCard::LandCard()
+LandCard::LandCard(Color color, const	std::string marker, const std::string texture, const bool isCreature,
+				   const double scaleX, const double scaleY, const double scaleZ, const double positionX, const double positionY, const double positionZ)
 {
+	color_ = color;
+	marker_ = marker;
+	texture_ = texture;
+	isCreature_ = isCreature;
+	
+	scaleX_ = scaleX;
+	scaleY_ = scaleY;
+	scaleZ_ = scaleZ;
 
+	positionX_ = positionX;
+	positionY_ = positionY;
+	positionZ_ = positionZ;
 }
 
 LandCard::~LandCard()
